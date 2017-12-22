af6a82b786f63ece86fa00677f7fd12cce2476fe
Joffreynf76 <32874652+Joffreynf76@users.noreply.github.com>
2017-12-22 17:32:46 +0100

    22/12

---
+++ PHP/PHP/PHP6/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    
+    
+    <title>Blog</title>
+    <link rel="stylesheet" href="./assets/css/normalize.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+   
+</head>
+<body>
+    <div id="main">
+    <?php
+    include_once("./include/header.php");
+        if (isset($_GET['page']) && $_GET['page'] != "") {
+            $page = $_GET['page'];    
+        }
+        
+        else {
+            $page = "default";
+        }
+        
+        $page = "./include/" . $page . ".inc.php";
+        $incFiles=glob("./include/*.inc.php");
+        if(in_array($page,$incFiles)){
+            include($page);
+        } else {
+            include("./include/default.inc.php");
+        }
+        
+        
+        include_once("./include/footer.php");
+        
+    ?>
+    </div>
+</body>
+</html>
